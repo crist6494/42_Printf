077687538658aaf99a7b0b9ddd771cc38193d437
crist6494 <moralesrojascr@gmail.com.com>
2022-05-26 18:42:16 +0200

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 19:13:06 by cmorales          #+#    #+#             */
-/*   Updated: 2022/05/25 20:45:20 by cmorales         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:40:51 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,35 @@
 
 int	ft_printf(char const *str, ...)
 {
-	va_start(str)
+	int	count = 0;
+	
+	va_list arglist;
+	va_start(arglist, str);
+	while(*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			if (*str == 's')
+				ft_putstr_fd(va_arg(arglist, char *),1, count);
+			if (*str == '%')
+				ft_putchar_fd('%', 1);
+			if (*str == 'd')
+				ft_putnbr_fd(va_arg(arglist, int),1);
+		}
+		else
+		{
+			ft_putchar_fd(*str, 1);
+			count++;
+		}
+		str++;
+	}
+	va_end(arglist);
+	return (count);	
+}
+
+int	main()
+{
+	ft_printf("hola buenas tardes %d", 1234);
+	return (0);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 19:36:03 by cmorales          #+#    #+#             */
-/*   Updated: 2022/05/25 20:44:10 by cmorales         ###   ########.fr       */
+/*   Updated: 2022/05/26 17:35:21 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-#include "../libft/libft.h"
+#include "./libft/libft.h"
 
 int	ft_printf(char const *str, ...);
 
